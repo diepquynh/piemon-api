313452ab5e1015eb20a5d9e6dc6d5fb888c0e4d0
Diep Quynh <remilia.1505@gmail.com>
2022-01-18 20:41:17 +0700

    user: controller: Get logged in user information
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- src/main/java/com/lillya/piemon/auth/user/controller/UserController.java
+++ src/main/java/com/lillya/piemon/auth/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.lillya.piemon.auth.user.controller;
 
 import com.lillya.piemon.auth.exception.UserAlreadyExistException;
+import com.lillya.piemon.auth.user.data.UserDAO;
 import com.lillya.piemon.auth.user.model.User;
 import com.lillya.piemon.auth.user.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/users")
 public class UserController {
     private final UserDetailsServiceImpl userDetailsService;
+    private final UserDAO userDAO;
 
     @Autowired
-    public UserController(UserDetailsServiceImpl userDetailsService) {
+    public UserController(UserDetailsServiceImpl userDetailsService, UserDAO userDAO) {
         this.userDetailsService = userDetailsService;
+        this.userDAO = userDAO;
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public ResponseEntity getUserInfo() {
+        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDAO.findByUsername(username);
+
+        return ResponseEntity.ok().body(user);
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
